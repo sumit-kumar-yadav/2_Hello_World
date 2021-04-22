0fb95f99c85c92db259403a51330b827a1bcf381
Sumit <sumityadav0376@gmail.com>
2021-04-22 13:14:32 +0530

    More on JsX & Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Sumit';
+  const isLoggedIn = false;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
+        <p>Hello {name}</p>
+        <p>Today's date is: {new Date().toDateString()}</p>
+        <p>Hello {isLoggedIn ? name : 'World'}</p>
+        {!isLoggedIn && <p>Hello World</p>}
+        {isLoggedIn && <p>Hello {name}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
